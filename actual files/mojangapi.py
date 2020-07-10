74ff0bb993391f1470ae9faf3be75a31b0c1e954
FatDubs <59372145+FatDubs@users.noreply.github.com>
2020-07-10 18:19:25 -0400

    Update mojangapi.py

--- actual files/mojangapi.py
+++ actual files/mojangapi.py
@@ -1,1 +1,12 @@
+import requests
+import json
 
+api_timeout = 3 # in seconds
+
+def getUUID(user):
+    try:
+        response = requests.get(f"https://api.mojang.com/users/profiles/minecraft/{player}",timeout=api_timeout).json()
+        return response["id"]
+    except:
+        logging.error("API Timeout! (mojang)")
+        return None
