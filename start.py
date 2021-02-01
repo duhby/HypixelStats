ed5efb394909957b3a3b6eaefd04ab28a96c9b81
dubs <59372145+FatDubs@users.noreply.github.com>
2021-02-01 10:45:50 -0500

    Create start.py

---
+++ start.py
@@ -0,0 +1,17 @@
+# dubs#9025
+# Hypixel Stats
+# 1/31/21
+
+import yaml
+import bot
+##TODO: add logging
+#import logging
+
+def main():
+    config = yaml.safe_load(open(config.yml))
+    bot = bot.thread(config['email'],config['password'],config['rate'])
+    bot.start()    
+
+
+if __name__ == "__main__":
+    main()
