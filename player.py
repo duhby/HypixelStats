4a608641dd1e169d601bc6f50ddf9b21ef8592fc
dubs <59372145+FatDubs@users.noreply.github.com>
2021-02-01 16:53:44 -0500

    Create player.py - see description for details
    Created player.py as a simple API wrapper for Hypixel. Added most of the more important gamemodes, however most of them will not be supported for the 2.0.0 release. See file for details.

---
+++ player.py
@@ -0,0 +1,230 @@
+# dubs#9025
+# Hypixel Stats
+# 2/1/21
+
+
+## If there is a "#UNSUPPORTED#" above a class, then that class (and all subsequent classes)
+## are not currently planned on being supported for 2.0.0s release.
+## If you, or somone you know who would find any of the unsupported gamemodes useful,
+## suggest it in my discord or DM me @dubs#9025.
+
+
+class player:
+    def __init__(self, player):
+        self.player = player
+
+
+    class arcade:
+        class captureTheWool:
+            None
+
+        class hypixelSays:
+            None
+
+        class miniWalls:
+            None
+
+        class partyGames:
+            None
+
+        #UNSUPPORTED# (for the remainder of the arcade gamemodes)
+
+
+    #UNSUPPORTED#
+    class arenaBrawl:
+        None
+
+
+    class bedwars:
+        class solo:
+            None
+
+        class doubles:
+            None
+
+        class threes:
+            None
+
+        class fours:
+            None
+
+
+        #UNSUPPORTED#
+        class dreams:
+            class armed:
+                None
+
+            class castle:
+                None
+
+            class lucky:
+                None
+
+            class rush:
+                None
+
+            class ultimate:
+                None
+
+            class voidless:
+                None
+
+
+    #UNSUPPORTED#
+    class blitz:
+        None
+
+
+    #UNSUPPORTED#
+    class buildBattle:
+        None
+
+
+    #UNSUPPORTED#
+    class copsAndCrims:
+        None
+
+
+    #UNSUPPORTED#
+    class crazyWalls:
+        None
+
+
+    class duels:
+        class blitz:
+            None
+
+        class bow:
+            None
+
+        class bridge:
+            None
+
+        class classic:
+            None
+
+        class noDebuff:
+            None
+
+        class op:
+            None
+
+        class skywars:
+            None
+
+        class sumo:
+            None
+
+        class uhc:
+            None
+
+
+    class megaWalls:
+        None
+
+
+    class murderMystery:
+        class classic:
+            None
+
+        #UNSUPPORTED#
+        class assassins:
+            None
+
+        #UNSUPPORTED#
+        class doubleUp:
+            None
+
+        #UNSUPPORTED#
+        class infection:
+            None
+
+
+    #UNSUPPORTED#
+    class paintball:
+        None
+
+
+    #UNSUPPORTED#
+    class quake:
+        None
+
+
+    #UNSUPPORTED#
+    class skyclash:
+        None
+
+
+    class skywars:
+        class ranked:
+            None
+
+        class soloNormal:
+            None
+
+        class soloInsane:
+            None
+
+        class teamNormal:
+            None
+
+        class teamInsane:
+            None
+
+
+    #UNSUPPORTED#
+    class smashHeroes:
+        None
+
+
+    #UNSUPPORTED#
+    class speedUHC:
+        None
+
+
+    class tkr:
+        None
+
+
+    class tntGames:
+        class tntRun:
+            None
+
+        #UNSUPPORTED#
+        class pvpRun:
+            None
+
+        class tntTag:
+            None
+
+        #UNSUPPORTED#
+        class bowSpleef:
+            None
+
+        #UNSUPPORTED#
+        class Wizards:
+            None
+
+
+    class UHC:
+        class solo:
+            None
+
+        #UNSUPPORTED#
+        class team:
+            None
+
+
+    #UNSUPPORTED#
+    class vampirez:
+        None
+
+
+    #UNSUPPORTED#
+    class walls:
+        None
+
+
+    #UNSUPPORTED#
+    class warLords:
+        None
