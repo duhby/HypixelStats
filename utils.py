f09e0bcc0fccb3ccf6964a8aea19f2ca35fad9af
BuildTools <unconfigured@null.spigotmc.org>
2021-02-06 17:49:53 -0500

    Add file read functions

--- utils.py
+++ utils.py
@@ -2,5 +2,14 @@
 # Hypixel Stats
 # 1/31/21
 
+import json
+
 class Files:
-    pass
+    def get_json(path):
+        with open(path, 'r') as file:
+            data = json.load(file)
+        return data
+
+    def write_json(path, data):
+        with open(path, 'w') as file:
+            json.dump(data, file)
